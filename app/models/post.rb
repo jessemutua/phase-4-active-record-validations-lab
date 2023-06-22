ed0e35072cff03d79aaa2c3126a9d25a8e67654e
Jesse <jessemutua76@gmail.com>
2023-06-22 12:50:10 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'], message: "is not included in the list" }
+
+    validate :sufficiently_clickbait_title
+    validates :summary, length: { maximum:250 }
+
+
+    private
+    def sufficiently_clickbait_title
+        unless title.present? && title.match?(/(Won't Believe|Secret|Top \d+|Guess)/)
+            errors.add(:title, "must be sufficiently clickbait-y")
+        end
+    end
+
 end
